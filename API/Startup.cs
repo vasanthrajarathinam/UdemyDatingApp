2609c75afe42e5d6d761ff79ce013c4e0c48cbaf
vasanth rajarathinam <vasanth.rajarathinam@hotmail.com>
2022-04-25 12:24:47 +0530

    section 3 end

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,7 @@ namespace API
             _config = config;
         }
 
-       
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -42,6 +42,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +58,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
